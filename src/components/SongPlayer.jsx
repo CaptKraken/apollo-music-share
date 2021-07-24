397a5e002db3cbb9d382fcd05ddeadf2815ca7c5
Song Kim <kimsong432@gmail.com>
2021-07-24 18:55:45 +0700

    finished

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -13,6 +13,10 @@ import { useContext } from "react";
 import { SongContext } from "../App";
 import { GET_QUEUED_SONGS } from "../graphql/queries";
 import QueuedSongList from "./QueuedSongList";
+import ReactPlayer from "react-player";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -46,9 +50,57 @@ const SongPlayer = () => {
   const { data } = useQuery(GET_QUEUED_SONGS);
   const { state, dispatch } = useContext(SongContext);
   const classes = useStyles();
+  const [played, setPlayed] = useState(0);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const [seeking, setSeeking] = useState(false);
+  const reactPlayerRef = useRef();
+
+  const [positionInQueue, setPositionInQueue] = useState(0);
+  useEffect(() => {
+    const songIndex = data.queue.findIndex((song) => song.id === state.song.id);
+    setPositionInQueue(songIndex);
+  }, [data.queue, state.song.id]);
+
+  useEffect(() => {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (played === 1 && nextSong) {
+      setPlayed(0);
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
+  }, [data.queue, played, dispatch, positionInQueue]);
 
   const handleTogglePlay = () => {
     dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+  };
+
+  const handleProgressChange = (e, newValue) => {
+    setPlayed(newValue);
+  };
+  const handleSeekMouseDown = () => {
+    setSeeking(true);
+  };
+  const handleSeekMouseUp = () => {
+    reactPlayerRef.current.seekTo(played);
+    setSeeking(false);
+  };
+
+  const formatDuration = (seconds) => {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+  };
+
+  const handlePlayPrevious = () => {
+    const previousSong = data.queue[positionInQueue - 1];
+    if (previousSong) {
+      setPlayed(0);
+      dispatch({ type: "SET_SONG", payload: { song: previousSong } });
+    }
+  };
+  const handlePlayNext = () => {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (nextSong) {
+      setPlayed(0);
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
   };
 
   return (
@@ -65,7 +117,7 @@ const SongPlayer = () => {
           </CardContent>
 
           <div className={classes.controls}>
-            <IconButton>
+            <IconButton onClick={handlePlayPrevious}>
               <SkipPrevious />
             </IconButton>
             <IconButton onClick={handleTogglePlay}>
@@ -75,16 +127,36 @@ const SongPlayer = () => {
                 <PlayArrow className={classes.playIcon} />
               )}
             </IconButton>
-            <IconButton>
+            <IconButton onClick={handlePlayNext}>
               <SkipNext />
             </IconButton>
             <Typography variant="subtitle1" component="p" color="textSecondary">
-              {state.song.duration}
+              {formatDuration(playedSeconds)}
             </Typography>
           </div>
-          <Slider type="range" min={0} max={1} step={0.01} />
+          <Slider
+            value={played}
+            onMouseDown={handleSeekMouseDown}
+            onMouseUp={handleSeekMouseUp}
+            onChange={handleProgressChange}
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+          />
         </div>
-
+        <ReactPlayer
+          onProgress={({ played, playedSeconds }) => {
+            if (!seeking) {
+              setPlayed(played);
+              setPlayedSeconds(playedSeconds);
+            }
+          }}
+          ref={reactPlayerRef}
+          url={state.song.url}
+          playing={state.isPlaying}
+          hidden
+        />
         <CardMedia image={state.song.thumbnail} className={classes.thumbnail} />
       </Card>
       <QueuedSongList queue={data?.queue} />
